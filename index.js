0bcdda08456b5b26ca100462c887e34bf13595a4
Monet Cooper <smomoc@Monets-MacBook-Pro-3.local>
2020-06-11 20:03:01 -0400

    updated code

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 const apiKey = "563492ad6f91700001000001bce3f0f7f72942048eea4da3cb90d3b0";
 
 function getPhoto(query){
-    fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
+fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=16&page=1`,
     {   
         method:"GET",
         headers:{
+            Accept: 'application/json',
             Authorization: apiKey
         }
     }
@@ -16,19 +17,28 @@ function getPhoto(query){
 .catch(error => alert('Something went wrong! Try again later'));
 }
 
+
 console.log(getPhoto());
 
 function displayResults(responseJson) {
     console.log(responseJson);
-    
+    $('.gallery').empty();
+  // loop throught the photos array 
+  
+    for (let i = 0; i < responseJson.photos.length; i++){
     $('.gallery').append(`
-        <img src="${photos.src.large}">
-        <p>${photos.photographer}</p>
-        `);
-        // <p><a href="${photos.url}">Download!</p>
-    //   $('.gallery').removeClass('hidden');
-} 
+        <div id="spacing">
+       <img src="${responseJson.photos[i].src.large}" class="img-search"> <br>
+       <p>${responseJson.photos[i].photographer}</p>
+       <a href="${responseJson.photos[i].url}">Download!</a>
+       </div>
+    `)
+    };
 
+    // $('.nav-button').append(`
+    // <a href="${responseJson.next_page}">More</a>
+    // `)
+} 
 
 function watchForm() {
     $('form').submit(event => {
