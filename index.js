b46df18c8793842568d73943385b4479ead9f817
Monet Cooper <smomoc@Monets-MacBook-Pro-3.local>
2020-06-12 22:16:36 -0400

    code updates

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
 "use strict";
 
 const apiKey = "563492ad6f91700001000001bce3f0f7f72942048eea4da3cb90d3b0";
+let page = 1;
+const url = 'https://api.pexels.com/v1/curated?per_page=16&page=5';
 
-function getPhoto(query){
-fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=16&page=1`,
-    {   
-        method:"GET",
-        headers:{
-            Accept: 'application/json',
-            Authorization: apiKey
-        }
+
+function getPhoto(query,pageNum,showMore = false){
+    let pageNumber;
+
+    if (!pageNum){
+        pageNumber = 1;
+    } else{
+        pageNumber = pageNum;
     }
-)
-.then(response => response.json())
-.then(responseJson => displayResults(responseJson))
-.catch(error => alert('Something went wrong! Try again later'));
+
+    let photoUrl;
+
+    if(!query){
+        photoUrl = url;
+    } else{
+        photoUrl = `https://api.pexels.com/v1/search?query=${query}+query&per_page=16&page=${pageNumber}`
+    }
+    
+    fetch(photoUrl,
+        {   
+            method:"GET",
+            headers:{
+                Accept: 'application/json',
+                Authorization: apiKey
+            }
+        }
+    )
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson, showMore))
+    .catch(error => alert('Something went wrong! Try again later'));
 }
 
+// function getCurated(){
+//     fetch('https://api.pexels.com/v1/curated?per_page=16&page=1',
+//         {   
+//             method:"GET",
+//             headers:{
+//                 Accept: 'application/json',
+//                 Authorization: apiKey
+//             }
+//         }
+//     )
+//     .then(response => response.json())
+//     .then(responseJson => displayResults(responseJson))
+//     .catch(error => alert('Something went wrong! Try again later'));
+//  }
 
-console.log(getPhoto());
 
-function displayResults(responseJson) {
-    console.log(responseJson);
-    $('.gallery').empty();
+function displayResults(responseJson, showMore = false) {
+    if (showMore === false){
+        $('.gallery').empty();
+    }
+
   // loop throught the photos array 
   
     for (let i = 0; i < responseJson.photos.length; i++){
@@ -34,18 +68,25 @@ function displayResults(responseJson) {
        </div>
     `)
     };
-
-    // $('.nav-button').append(`
-    // <a href="${responseJson.next_page}">More</a>
-    // `)
 } 
+
+function moreImages(){
+   
+    $('#more').click(event =>{
+    let search = $('#search_input').val();
+     getPhoto(search,++page,true);
+    });
+}
 
 function watchForm() {
     $('form').submit(event => {
       event.preventDefault();
-      let search = $('input[name="search"]').val();
+      let search = $('#search_input').val();
       getPhoto(search);
+    //   $('#search_input').val('');
     });
+    moreImages();
+    getPhoto();
   }
   
   $(function() {
@@ -53,3 +94,5 @@ function watchForm() {
     // console.log(watchForm());
     watchForm();
   });
+
+
