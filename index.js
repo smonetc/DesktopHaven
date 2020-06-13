475c6227a741a48ee5acecc781e762dca399ae47
Monet Cooper <smomoc@Monets-MacBook-Pro-3.local>
2020-06-13 01:17:04 -0400

    updated html css and js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const apiKey = "563492ad6f91700001000001bce3f0f7f72942048eea4da3cb90d3b0";
 let page = 1;
-const url = 'https://api.pexels.com/v1/curated?per_page=16&page=5';
+// commented out const url because I wanted the user to be able to
+//  view additional pages before searching for something specific 
+// const url = 'https://api.pexels.com/v1/curated?per_page=16&page=5';
 
 
 function getPhoto(query,pageNum,showMore = false){
@@ -17,7 +19,7 @@ function getPhoto(query,pageNum,showMore = false){
     let photoUrl;
 
     if(!query){
-        photoUrl = url;
+        photoUrl = `https://api.pexels.com/v1/curated?per_page=16&page=${pageNumber}`;
     } else{
         photoUrl = `https://api.pexels.com/v1/search?query=${query}+query&per_page=16&page=${pageNumber}`
     }
@@ -36,20 +38,6 @@ function getPhoto(query,pageNum,showMore = false){
     .catch(error => alert('Something went wrong! Try again later'));
 }
 
-// function getCurated(){
-//     fetch('https://api.pexels.com/v1/curated?per_page=16&page=1',
-//         {   
-//             method:"GET",
-//             headers:{
-//                 Accept: 'application/json',
-//                 Authorization: apiKey
-//             }
-//         }
-//     )
-//     .then(response => response.json())
-//     .then(responseJson => displayResults(responseJson))
-//     .catch(error => alert('Something went wrong! Try again later'));
-//  }
 
 
 function displayResults(responseJson, showMore = false) {
@@ -61,11 +49,13 @@ function displayResults(responseJson, showMore = false) {
   
     for (let i = 0; i < responseJson.photos.length; i++){
     $('.gallery').append(`
-        <div id="spacing">
-       <img src="${responseJson.photos[i].src.large}" class="img-search"> <br>
-       <p>${responseJson.photos[i].photographer}</p>
-       <a href="${responseJson.photos[i].url}">Download!</a>
-       </div>
+       <div id="spacing">
+        <img src="${responseJson.photos[i].src.medium}" class="img-search"> <br>
+        <div id="photograph-info">
+            <p>${responseJson.photos[i].photographer}</p>
+            <a href="${responseJson.photos[i].url}"><i class="fas fa-file-download"></i></a>
+        </div>
+        </div>
     `)
     };
 } 
